014b42b9b961f3c0dd69cc44074deeaa5916e4ca
MariaLuizaCavalcanti <maria.luiza.andrade.cavalcanti@ccc.ufcg.edu.br>
2025-03-22 16:01:26 -0300

    ajustes de mostrarObjetivos

--- mostarObjetivos.pl
+++ mostarObjetivos.pl
@@ -1,50 +1,34 @@
 :- use_module(library(readutil)).
-
-clear_screen:-
-    shell('clear', _).
+:- use_module(library(random)).
 
 objetivos([
-    "Conquistar QUATORZE TERRITÓRIOS à sua escolha.",
-    "Conquistar DOZE TERRITÓRIOS à sua escolha e ocupar cada um deles com pelo menos dois exércitos.",
-    "Conquistar na totalidade a AMÉRICA e a OCEANIA.",
-    "Conquistar na totalidade a ÁSIA e a EUROPA.",
-    "Conquistar na totalidade a EUROPA, a OCEANIA e a ÁFRICA.",
-    "Conquistar na totalidade a AMÉRICA e a EUROPA.",
-    "Seu objetivo é fazer com que o JOGADOR 2 seja ELIMINADO. Caso você seja o jogador dois, seu objetivo passa a ser conquistar QUATORZE TERRITÓRIOS à sua escolha."
-]). 
+    "Conquistar QUATORZE TERRITORIOS a sua escolha.",
+    "Conquistar DOZE TERRITORIOS a sua escolha e ocupar cada um deles com pelo menos dois exercitos.",
+    "Conquistar na totalidade a AMERICA e a OCEANIA.",
+    "Conquistar na totalidade a ASIA e a EUROPA.",
+    "Conquistar na totalidade a EUROPA, a OCEANIA e a AFRICA.",
+    "Conquistar na totalidade a AMERICA e a EUROPA.",
+    "Seu objetivo e fazer com que o JOGADOR 2 seja ELIMINADO. Caso voce seja o jogador dois, seu objetivo passa a ser conquistar QUATORZE TERRITORIOS a sua escolha."
+]).
 
-expor_objetivo:- 
-/passar a quantidade/
-    write("Agora vamos mostrar os objetivos. Se prepare!"), nl,
-    write("Pressione ENTER para ver os objetivos"), nl,
-    read_line_to_codes(user_input, _),
-    clear_screen,
-    objetivos(Lista),
-    /expor_objetivo_rec(seria a quantidade, 1, Lista/
-    .
+embaralhar_objetivos(Shuffled) :-
+    objetivos(List),
+    random_permutation(List, Shuffled).
 
-expor_objetivo_rec(_,_,[]):-
-    write("Todos os objetivos foram exibidos."), nl,
-    write("Pressione ENTER para continuar"), nl,
-    read_line_to_codes(user_input, _),
-    clear_screen.
+atribuir_objetivos(Qtd) :-
+    embaralhar_objetivos(Shuffled),
+    length(ObjetivosDistribuidos, Qtd), 
+    append(ObjetivosDistribuidos, _, Shuffled),  
+    distribuir_objetivos(ObjetivosDistribuidos, 1). 
 
-/precisa receber a quantidade e um indice/
-expor_objetivo_rec(, , [H|T]):-
-    (Indice > Qtd -> 
-        format("Pressione ENTER para continuar"), nl,
-        read_line_to_codes(user_input, _),
-        clear_screen,
-        format("Todos os objetivos foram exibidos."), nl
-    ;
-        format("Pressione ENTER para ver o objetivo do jogador ~w", [Indice]), nl,
-        read_line_to_codes(user_input, _),
-        clear_screen,
-        /passar o indice/
-        format("Objetivo do jogador ~w", []), nl,
-        /o objetivo que estiver na cabeça da lista/
-        format("Objetivo: ~w", [H]), nl,
-        /passar a quantidade e o indice + 1/
-        expor_objetivo_rec(, , T)
-    ).
-    
+distribuir_objetivos([], _) :-
+    writeln("Todos os objetivos foram exibidos.").
+
+distribuir_objetivos([], _).
+distribuir_objetivos([H|T], Indice) :-
+    format('Pressione ENTER para verificar o objetivo do Jogador ~w.~n', [Indice]),
+    get_char(_),
+    format('Objetivo do Jogador ~w: ~w~n', [Indice, H]), nl,
+    NovoIndice is Indice + 1,
+    distribuir_objetivos(T, NovoIndice).
+
