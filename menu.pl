dacc8b815c375d572e485f585f29a2337a4f6244
Gabriellalcaraujo <140713498+Gabriellalcaraujo@users.noreply.github.com>
2025-03-22 09:35:30 -0300

    iniciando o projeto

--- menu.pl
+++ menu.pl
@@ -3,16 +3,19 @@ validaJogadores(QtdJog):-
     Opcoes = [1, 2, 3, 4],
     member(QtdJog, Opcoes).
 maxBots(QtdJog, R):- R is 4-QtdJog. 
-validaBots(QtdBots, Q):- validaJogadores(QtdBots), QtdBots =< Q, QtdBots >= 1.
+validaBots(QtdBots, Q):- validaJogadores(QtdBots), QtdBots =< Q.
 
 
 menu:-
-    write("Você deseja começar um novo jogo (0) ou continuar um jogo salvo (1)?")
+    write("Você deseja começar um novo jogo (0) ou continuar um jogo salvo (1)?"),
     read(Desejo),
     (\+ verificaOpcao(Desejo)-> write("Entrada Inválida :("), menu ; true),
-    write("Quantos jogadores terão na partida? (1 a 4):"),
+    repeat,
+    write("Quantos jogadores terão na partida? (1 a 4): "), nl,
     read(QtdJog),
-    (\+ verificaOpcao(Desejo)-> write("Entrada Inválida :(", nl, ), write("Quantos jogadores terão na partida? (1 a 4):"),  ; true),
-    qtdBots(QtdJog, R),
+    (validaJogadores(QtdJog) -> ! ; write("Entrada Inválida! Tente novamente."), nl, fail),
+    maxBots(QtdJog, R),
+    repeat,
     format("Quantos bots seu jogo terá? (~w)~n", [R]),
-    read(QtdBots).
+    read(QtdBots),
+    (validaBots(QtdBots, R) -> ! ; write("Entrada Inválida! Tente novamente."), nl, fail).
