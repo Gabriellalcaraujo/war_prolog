00f4e75335334d3156d8f263cafe04ba53859286
Gabriellalcaraujo <140713498+Gabriellalcaraujo@users.noreply.github.com>
2025-03-28 17:28:14 -0300

    distribuicao de territorios atualizada

--- distribuicaoTerritorios.pl
+++ distribuicaoTerritorios.pl
@@ -5,10 +5,9 @@ territorios([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 
 
 mapa([[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]]). 
 
-substituirSublista([_|T], 0, NovaSublista, [NovaSublista|T]).
-
-substituirSublista([_|T], Pos, NovaSublista, [_|Resultado]) :- 
-    Pos > 0,
+substituirSublista([_|T], 1, NovaSublista, [NovaSublista|T]).
+substituirSublista([H|T], Pos, NovaSublista, [H|Resultado]) :- 
+    Pos > 1,
     NextPos is Pos - 1,
     substituirSublista(T, NextPos, NovaSublista, Resultado).
 
@@ -19,12 +18,38 @@ embaralhar_territorios(Shuffled) :-
 posicionamento_inicial(Qtd, NovoMapa) :-
     embaralhar_territorios(TerritoriosEmbaralhados),
     mapa(MapaInicial),
-    distribuir_territorios(TerritoriosEmbaralhados, 1, Qtd, MapaInicial, NovoMapa).
+    distribuir_territorios(TerritoriosEmbaralhados, MapaInicial, Qtd, 1, NovoMapa).
 
-distribuir_territorios([], _, _, _, Mapa, Mapa). 
+distribuir_territorios([], MapaAtualizado, _, _, MapaAtualizado). 
+distribuir_territorios([H|T], Mapa, Qtd, Ind, NovoMapa) :-
+    determinaJogador(Qtd, Ind, R),
+    substituirSublista(Mapa, H, [R, 1], MapaAtualizado),
+    Ind1 is Ind+1,
+    distribuir_territorios(T, MapaAtualizado, Qtd, Ind1, NovoMapa).
+    
+determinaJogador(Qtd, Ind, R):-
+    determinaJogadorRec(Qtd, Ind, 1, R).
 
-distribuir_territorios([H|T], JogadorAtual, Qtd, MapaAtual, NovoMapa) :-,
-    substituirSublista(MapaAtual, Index, [JogadorAtual, H], MapaAtualizado),
-    ProximoJogador is (JogadorAtual mod Qtd) + 1,
-    ProximoIndex is Index + 1,
-    distribuir_territorios(T, ProximoJogador, Qtd, MapaAtualizado, NovoMapa).
+determinaJogadorRec(Qtd, Idc, Njogador, R):-
+    Nterr is 24//Qtd,
+    (Nterr* Njogador >= Idc -> R = Njogador, !; Njogador1 is Njogador+1, determinaJogadorRec(Qtd, Idc, Njogador1, R)).
+
+main :-
+    write('Teste de embaralhamento de territórios...'), nl,
+    % Teste do embaralhamento dos territórios
+    embaralhar_territorios(Shuffled),
+    write('Territórios embaralhados: '), write(Shuffled), nl,
+    
+    write('Teste de substituição de sublista...'), nl,
+    % Teste de substituição de sublista (vamos substituir o território 5 por [1, 1])
+    mapa(MapaInicial),
+    substituirSublista(MapaInicial, 4, [1, 1], MapaSubstituido),
+    write('Mapa após substituição: '), write(MapaSubstituido), nl,
+
+    determinaJogador(2, 0, R),
+    write(R),
+    
+    write('Teste de distribuição de territórios...'), nl,
+    % Teste da função de distribuição de territórios
+    posicionamento_inicial(2, NovoMapa),
+    write('Mapa final: '), write(NovoMapa), nl.
