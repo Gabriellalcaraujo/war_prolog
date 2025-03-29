ee5619d5df685797bd1b1b02d72b45b4bd8f98fc
MariaLuizaCavalcanti <maria.luiza.andrade.cavalcanti@ccc.ufcg.edu.br>
2025-03-29 13:38:35 -0300

    fazendo mapa colorido

--- representacaoTerritorios.pl
+++ representacaoTerritorios.pl
@@ -11,31 +11,49 @@ define_cor(_) :- write('\033[35m').   % Magenta
 
 reset_color :- write('\033[0m').
 
-imprime_siglas_colorido(Indice, Territorio) :-
-    define_cor(Indice),
-    retornaSigla(Territorio, Sigla),
+imprime_siglas_colorido(Jogador, Indice) :-
+    define_cor(Jogador),
+    retornaSigla(Indice, Sigla), 
     write(Sigla),
     reset_color. 
 
-imprime_numero_colorido(Indice, Numero) :-
-    define_cor(Indice),
+imprime_numero_colorido(Jogador, Numero) :-
+    define_cor(Jogador),
     % format_number(Numero, NumeroFormatado),
     % write(NumeroFormatado),
     write(Numero),
     reset_color.
 
-imprime_mapa_formatado(Mapa) :-
+imprime_mapa_colorido(Mapa) :-
     write("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣄⣠⣀⡀⣀⣠⣤⣤⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"), nl,
     write("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣄⢠⣠⣼⣿⣿⣿⣟⣿⣿⣿⣿"),
-    nth0(2, Mapa, [J1, T1]),
-    imprime_siglas_colorido(J1, T1),
+
+    nth0(2, Mapa, [J1, E1]),
+    imprime_siglas_colorido(J1, 3),
     write("⣿⣿⡿⠋⠀⠀⠀⢠⣤⣦⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠰⢦⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"), nl,
     write("*⠀⠀⠀⠀⠀⠀⠀⣼⣿⣟⣾⣿⣽⣿⣿⣅⠈⠉⠻⣿"),
-    nth0(2, Mapa, [J2, N2]),
-    imprime_numero_colorido(J2, N2),
+
+    imprime_numero_colorido(J1, E1),
     write("⣿⡿⠇*⠀⠀⠀⠀⠉⠀⠀⠀⠀⠀⢀⡶⠒⢉⡀⢠⣤⣶⣶⣿⣷⣆⣀⡀⠀⢲⣖⠒⠀⠀⠀⠀⠀⠀*"), nl,
     write("⢀⣤⣾⣶⣦⣤⣤⣶⣿⣿⣿⣿⣿⣿⣽⡿⠻⣷⣀⠀⢻⣿⣿⣿⡿⠟⠀ * ⠀⠀⣤⣶⣶⣤⣀⣀⣬⣷⣦⣿⣿⣿⣿⣿⣿⣿"),
-    nth0(16, Mapa, [J3, T3]),
-    imprime_siglas_colorido(J3, T3),
+    
+    nth0(16, Mapa, [J3, E3]),
+    imprime_siglas_colorido(J3, 17),
     write("⣿⣿⣿⣿⣿⣿⣶⣦⣤⣦⣼⣀⠀"), nl,
-    write("⠈⣿").
+    write("⠈⣿"),
+
+    nth0(0, Mapa, [J4, E4]),
+    imprime_siglas_colorido(J4, 1),
+    write("⣿⣿⣿⣿⣿⣿"),
+
+    nth0(1, Mapa, [J5, E5]),
+    imprime_siglas_colorido(J5, 2),
+    write("⣿⣿⡿⠛⠓⣿⣿⠟⠁⠘⣿⡟⠁⠀⠘⠛⠁⠀*⢠⣾⣿⢿⣿⣿⣿⣿"),
+
+    nth0(9, Mapa, [J6, E6]),
+    imprime_siglas_colorido(J6, 10),
+    write("⣿⣿⣿⣿⣿⣿⣿"), nl,
+
+    nth0(16, Mapa, [J7, E7]),
+    imprime_numero_colorido(E7, 17),
+    write("⣿⣿⣿⣿").
