bcae58f767828ef495cebc31e2e82182add42707
oscarhtb <oscar.henrique.torreao.vilar.brito@ccc.ufcg.edu.br>
2025-04-06 12:14:09 -0300

    continuacao do desenvolvimento do bot (quase pronto

--- botJogadas.pl
+++ botJogadas.pl
@@ -1,4 +1,7 @@
 :- use_module(mapeiaTerritorios).
+:- use_module(utilsAtaque).
+:- use_module(ataque).
+:- use_module(distribuicaoTerritorios).
 
 botJoga(Mapa, JogInfo, IndiceJogador, Objetivos, NovoMapa) :-
     possibilidadesDeAtaque(Mapa, IndiceJogador, Possibilidades),
@@ -13,7 +16,7 @@ botJoga(Mapa, JogInfo, IndiceJogador, Objetivos, NovoMapa) :-
         ;   
             format("O jogador ~w decidiu atacar.~n", [IndiceJogador]),
             random_member(Ataque, Possibilidades),
-            botAtaque(Mapa, IndiceJogador, Ataque, Objetivos, MapaAposAtaque), %FALTA DEFINIR COM QUANTOS EXÉRCITOS ELE VAI ATACAR
+            botAtaque(Mapa, IndiceJogador, Ataque, Objetivos, MapaAposAtaque),
             possibilidadesDeAtaque(MapaAposAtaque, IndiceJogador, NovasPossibilidades),
             ( NovasPossibilidades == [] -> 
                 format("O jogador ~w não tem mais ataques disponíveis e passou a vez.~n", [IndiceJogador]), 
@@ -24,11 +27,47 @@ botJoga(Mapa, JogInfo, IndiceJogador, Objetivos, NovoMapa) :-
         )
     ).
 
-botAtaque(Mapa, IndiceJogador, Ataque, Objetivos, MapaAposAtaque) :- MapaAposAtaque = Mapa.
+botAtaque(Mapa, IndiceJogador, Ataque, Objetivos, MapaF) :-
+    nth0(0, Ataque, Atacante),
+    writeln(Atacante), %log check
+    nth0(1, Ataque, Atacado),
+    writeln(Atacado), %log check
+    maxUtilExercitos(Mapa, Atacante, Max),
+    writeln(Max), %log check
+    nth1(Atacado, Mapa, TerrAtacado),
+    writeln(TerrAtacado), %log check
+    nth0(1, TerrAtacado, QtdTerrDef),
+    writeln(QtdTerrDef), %log check
+    min(QtdTerrDef, QtdDef),
+    writeln(QtdDef), %log
+    random_between(1, Max, QtdAtaque),
+    writeln(QtdAtaque), %log
+    QtdDados is QtdAtaque + QtdDef,
+    writeln(QtdDados), %log
+    format("O jogador ~w usou o território ~w para atacar o território ~w utilizando ~w exércitos.~n", [IndiceJogador, Atacante, Atacado, QtdAtaque]),
+    embaralhar_dados(QtdAtaque, QtdDados, DadosAtac, DadosDef),
+    format("DADOS DE ATAQUE: ~w~n", [DadosAtac]),
+    format("DADOS DE DEFESA: ~w~n~n", [DadosDef]),
+    formataDados(DadosAtac, DadosDef, PerdasAtaq, PerdasDef),
+    format("O jogador atacante perdeu ~w exércitos~n", [PerdasAtaq]),
+    format("O jogador defensor perdeu ~w exércitos~n", [PerdasDef]),
+    batalhaMapa(Mapa, PerdasAtaq, PerdasDef, Atacante, Atacado, NovoMapa),
+
+    (conquistouTerr(NovoMapa, Atacado) ->
+        maxUtilExercitos(NovoMapa, Atacante, NovoMax),
+        random_between(1, NovoMax, QtdTransf),
+        format("O bot conquistou o território e transferiu ~w exércitos para o território conquistado.~n", [QtdTransf]),
+        substituirSublista(NovoMapa, Atacado, [IndiceJogador, QtdTransf], MapaAtt),
+        jogQtdExercitos(MapaAtt, Atacante, QtdExercitos, _),
+        NovaQtdE is QtdExercitos - QtdTransf,
+        substituirSublista(MapaAtt, Atacante, [IndiceJogador, NovaQtdE], MapaF)
+    ;
+        MapaF = NovoMapa
+    ).
 
 
- %APARENTEMENTE FUNCIONANDO
- %retorna uma lista de pares, que são os possíveis ataques do bot
+
+
 possibilidadesDeAtaque(Mapa, IndiceJ, Possibilidades) :-
     findall([T1, T2], pode_atacar(Mapa, IndiceJ, T1, T2), Possibilidades).
 
