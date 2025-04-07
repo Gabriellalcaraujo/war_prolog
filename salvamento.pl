baf5e9abb99738d463cd5fe7d27fa5ee949b2591
anaramoscl <ana.luiza.cavalcante.ramos@ccc.ufcg.edu.br>
2025-04-07 18:17:57 -0300

    testado

--- salvamento.pl
+++ salvamento.pl
@@ -1,4 +1,4 @@
-:- module(salvamento, [salvarJogo/2, carregarJogo/2]).
+:- module(salvamento,[salvarJogo/2, carregarJogo/7]).
 
 salvarJogo(EstadoDoJogo, NomeArquivo) :-
     open(NomeArquivo, append, Stream),  
@@ -7,16 +7,14 @@ salvarJogo(EstadoDoJogo, NomeArquivo) :-
     close(Stream).            
 
 % Observar que essa função assume que o arquivo contem exatamente o estado do jogo
-carregarJogo(NomeArquivo, Mapa, JogadoresInfo, QtdBots, Objetivos, NumRodada, IndiceJogador) :-
+carregarJogo(NomeArquivo, MapaS, JogadoresInfoS, QtdBotsS, ObjetivosS, NumRodadaS, IndiceJogadorS) :-
     open(NomeArquivo, read, Stream),
     read(Stream, EstadoDoJogo),               
     close(Stream),
-    nth1(1,EstadoDoJogo,Mapa),
-    nth1(2,EstadoDoJogo, JogadoresInfo),
-    nth1(3,EstadoDoJogo, QtdBots),  
-    nth1(4,EstadoDoJogo,Objetivos),                  
-    nth1(5,EstadoDoJogo, NumRodada),
-    nth1(6,EstadoDoJogo, IndiceJogador).
+    nth1(1,EstadoDoJogo,MapaS),
+    nth1(2,EstadoDoJogo, JogadoresInfoS),
+    nth1(3,EstadoDoJogo, QtdBotsS),  
+    nth1(4,EstadoDoJogo,ObjetivosS),                  
+    nth1(5,EstadoDoJogo, NumRodadaS),
+    nth1(6,EstadoDoJogo, IndiceJogadorS).
 
-% testando com um mapa aleatório
-main:- carregarJogo('teste.txt',Mapa, JogadoresInfo, QtdBots, Objetivos, NumRodada, IndiceJogador),write(Mapa).
