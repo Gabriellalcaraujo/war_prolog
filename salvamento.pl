23adba2d4b1c493a8cb0e236574376bdeec838af
anaramoscl <ana.luiza.cavalcante.ramos@ccc.ufcg.edu.br>
2025-04-07 14:49:01 -0300

    Atualizando o salvamento

--- salvamento.pl
+++ salvamento.pl
@@ -7,11 +7,16 @@ salvarJogo(EstadoDoJogo, NomeArquivo) :-
     close(Stream).            
 
 % Observar que essa função assume que o arquivo contem exatamente o estado do jogo
-carregarJogo(NomeArquivo, EstadoDoJogo) :-
+carregarJogo(NomeArquivo, Mapa, JogadoresInfo, QtdBots, Objetivos, NumRodada, IndiceJogador) :-
     open(NomeArquivo, read, Stream),
     read(Stream, EstadoDoJogo),               
-    close(Stream).                     
-       
+    close(Stream),
+    nth1(1,EstadoDoJogo,Mapa),
+    nth1(2,EstadoDoJogo, JogadoresInfo),
+    nth1(3,EstadoDoJogo, QtdBots),  
+    nth1(4,EstadoDoJogo,Objetivos),                  
+    nth1(5,EstadoDoJogo, NumRodada),
+    nth1(6,EstadoDoJogo, IndiceJogador).
 
 % testando com um mapa aleatório
-main:- carregarJogo('teste.txt', EstadoDoJogo),write(EstadoDoJogo).
+main:- carregarJogo('teste.txt',Mapa, JogadoresInfo, QtdBots, Objetivos, NumRodada, IndiceJogador),write(Mapa).
