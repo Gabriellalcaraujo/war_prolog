29f6c7f7d468b74726d345ab40c8dee439cd30d2
Gabriellalcaraujo <140713498+Gabriellalcaraujo@users.noreply.github.com>
2025-04-10 15:12:02 -0300

    jogo finalizado

--- botJogadas.pl
+++ botJogadas.pl
@@ -36,21 +36,13 @@ botJoga(Mapa, JogInfo, IndiceJogador, Objetivos, NovoMapa) :-
 
 botAtaque(Mapa, IndiceJogador, Ataque, Objetivos, MapaF) :-
     nth0(0, Ataque, Atacante),
-    writeln(Atacante), %log check
     nth0(1, Ataque, Atacado),
-    % writeln(Atacado), %log check
     maxUtilExercitos(Mapa, Atacante, Max),
-    % writeln(Max), %log check
     nth1(Atacado, Mapa, TerrAtacado),
-    % writeln(TerrAtacado), %log check
     nth0(1, TerrAtacado, QtdTerrDef),
-    % writeln(QtdTerrDef), %log check
     min(QtdTerrDef, QtdDef),
-    % writeln(QtdDef), %log
     random_between(1, Max, QtdAtaque),
-    % writeln(QtdAtaque), %log
     QtdDados is QtdAtaque + QtdDef,
-    % writeln(QtdDados), %log,
     retornaSigla(Atacante, SiglaAtacante),
     retornaSigla(Atacado, SiglaAtacado),
     format("O jogador ~w usou o território ~w para atacar o território ~w utilizando ~w exércitos.~n", [IndiceJogador, SiglaAtacante, SiglaAtacado, QtdAtaque]),
